debd2b59ef3d9283b8fc73dcbd272cac09315218
Luiz <luiz.felipe.556@outlook.com>
2020-04-10 17:26:22 -0300

    class 15

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const chalk = require('chalk');
 const getNotes =  require('./notes');
 
-console.log(getNotes());
-console.log(chalk.red.inverse('Error!'));
+const command = process.argv[2];
 
+console.log(process.argv);
 
+if (command === 'add') {
+    console.log('Adding Note');
+} else if (command == 'remove') {
+    console.log('Removing note...');
+}
