4e963519578d7443f963cde214e7e43461c4d677
Luiz <luiz.felipe.556@outlook.com>
2020-04-10 19:11:47 -0300

    class 19

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const chalk = require('chalk');
 const yargs = require('yargs'); 
 
-const getNotes =  require('./notes');
+const notes =  require('./notes');
 
 // Customize yargs version
 yargs.version('1.1.0');
@@ -23,8 +23,7 @@ yargs.command({
         }
     },
     handler: function (argv) {
-        console.log('Title: ' + argv.title);
-        console.log('Description: ' + argv.body);
+        notes.addNote(argv.title, argv.body);
     }
 })
 
--- notes.js
+++ notes.js
@@ -1,6 +1,44 @@
+const fs = require('fs');
+
 const getNotes = () => {
     const str = 'Your notes...';
     return str;
 }
 
-module.exports = getNotes;
+const addNote = (title, body) => {
+    const notes = loadNotes();
+
+    const verify = notes.find(note => note.title === title);
+    if (verify) {
+        console.log('Note title taken!');
+        return;
+    }
+    
+    notes.push({
+        title: title,
+        body: body
+    });
+
+    saveNotes(notes);
+    console.log('New note added!');
+}
+
+const saveNotes = (notes) => {
+    const notesJSON = JSON.stringify(notes);
+    fs.writeFileSync('notes.json', notesJSON);
+}
+
+const loadNotes = () => {
+    try {
+        const dataBuffer = fs.readFileSync('notes.json');
+        const dataJSON = dataBuffer.toString();
+        return JSON.parse(dataJSON);
+    } catch (err) {
+        return [];
+    }
+}
+
+module.exports = {
+    getNotes,
+    addNote
+}
